bc332fffd141c2b16548606e593813fbbb965046
matan Kribus (aswe323) <matanwx135@gmail.com>
2018-05-27 18:51:19 +0300

    continue of meets

--- js/functions.js
+++ js/functions.js
@@ -14,17 +14,26 @@ step 4
 
 search based on email
 */
+//access to needed elements of the page
+//access to  div elemetn with .page-header class so to appened it the random page link	
+const divHeader = document.querySelector('.pages-header');
 const studentListDOM = document.querySelector(".student-list").children; // acces to list of li containing students
 //accessing the email and name of students
 const studentEmail = (student) => student.querySelector("div span").innerText;//access a studentListDOM email text
 const studentName = (student) => student.querySelector("div h3").innerText;//acces to studentListDOM name text
 
-function hideAndOrder (studentListDOM) {
+function hideAndAppened(studentListDOM) {
 	studentListDOM.style.display = "none";//adding a style to hide all the students from the html
+
+	//add an html addition every loop from a changing arrey,each arrey instence will be a set of diffrent students according the the order of original arrey	
 	for (let i; i<=Math.floor(studentListDOM/10);i++){
-	//create an html addition every loop in a changing arrey,each arrey instence will be a set of diffrent students according the the order of original arrey	
+
+	//changing arrey every loop		
 	let page = studentListDOM.splice(0, 10);
-	//access to page header class so to appened it the random page link	
+
+	//creat a button elemente
+	let button = document.createElement("button");
+	//attach the hidden students arrey instence to the button(say that 5 times xD)
 	}
 
 	
