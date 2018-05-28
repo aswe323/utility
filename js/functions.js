6b93fc2dd7c5f3c9138f0572a8e60c606c11af53
matan Kribus (aswe323) <matanwx135@gmail.com>
2018-05-28 23:14:26 +0300

    event listener is now on the buttons themeslefs

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@
  complete:make an exception in the creation of buttons for when the number of items in studentListDOM is not returning any leftovers when divided by 10
  TODO:look for a way to make the errors disapear. not even kidding.
  complete:eliminate any uneccesery single letter variables and make more clear once insted of them
- TODO:make the button event listener closer to the butotn (idealy in the buttons themself)
+ complete:make the button event listener closer to the butotn (idealy in the buttons themself)
 */ //access to needed elements of the page
 const divPage = document.querySelector('.page');//acces to first tag with page class in it. 
 const studentListDOM = document.querySelector(".student-list").children; // acces to list of li containing students
@@ -26,39 +26,43 @@ function buttons(){
 
 	for (let index=0;index<=studentListDOM.length/10;index++){
 		
-		if (studentListDOM.item(ndex+1) === null){break};
+		if (studentListDOM.item(index+1) === null){break};
 		let button = document.createElement("button");
 		console.log(button);
-		button.setAttribute("indexStart", index*10);
+		button.setAttribute("data-indexstart", index*10);
 		button.setAttribute("display", "inline");
 		button.innerHTML = index+1;
 		divPage.appendChild(button);
 	}
 };
-
+buttons();
+let buttonElements = document.querySelectorAll("[data-indexstart]");
+console.log(buttonElements);
+for (let buttonInstence=0;buttonInstence<buttonElements.length;buttonInstence++){
+	
 /*event listener will respong to clicks on elements with indexStart only, then will loop from
   indexStart forword 10 steps (inclusive) and set display to true.*/
-document.getElementsByTagName('body')[0].addEventListener('click', (event) =>{
+//document.getElementsByTagName('body')[0].addEventListener('click', (event) =>{
 
-	//if the target has idexstart attrbute then...
-	if (event.target.hasAttribute('indexStart')){
+	buttonElements[buttonInstence].addEventListener('click', (event) =>{
 
-	let button = event.target;//set the button variable to target
-	const buttonIndex = button.getAttribute('indexstart');//set button index to the index value
+		let button = event.target;//set the button variable to target
+		const buttonIndex = button.getAttribute('data-indexstart');//set button index to the index value
 
-	hideAll();//hides all the student elements
+		hideAll();//hides all the student elements
 
-	//if event index is not smaller then the starting index value plus 10. continue enabling student LI
-	for (let eventIndex=buttonIndex; eventIndex<(parseInt(buttonIndex)+10);eventIndex++){
-		studentListDOM.item(eventIndex).style.display='list-item';
-		}
-	}
-})
+		//if event index is not smaller then the starting index value plus 10. continue enabling student LI
+		for (let eventIndex=buttonIndex; eventIndex<(parseInt(buttonIndex)+10);eventIndex++){
+			studentListDOM.item(eventIndex).style.display='list-item';
+			}
+	
+	})
+}
 //calling all function
 function hideAndAppened() {
 
 	hideAll();
-	buttons();
+
 	//default page is the first. so we display the first 10 students
 	for (let i=0;i<10;i++){
 		studentListDOM.item(i).style.display ="list-item";
